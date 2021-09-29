19042c1a55f967329f1015102e04099d191f032d
Scott Maddock <scottowenmaddock@gmail.com>
2021-09-29 16:57:14 -0400

    Movie abstract

---
+++ SideProjectMovie/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SideProjectMovie
+{
+    public abstract class Movie
+    {
+        //properties
+        public abstract string Title { get; set; }
+        public string Director { get; set; } = "unknown";
+        public string Date { get; set; } = "unknown";
+        //constuctor
+        public Movie(string title )
+        {
+            Title = title;
+           // Director = director;
+        }
+        //methods
+
+
+    }
+}
--- SideProjectMovie/Program.cs
+++ SideProjectMovie/Program.cs
@@ -6,7 +6,9 @@ namespace SideProjectMovie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+
         }
     }
 }
